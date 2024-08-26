f41d0415f190157611280ebc1731ae0dd96f46f8
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2024-08-26 12:53:41 -0400

    server genome blast beginning

---
+++ Server_Genome_Blaster/server_genome_blaster.py
@@ -0,0 +1,15 @@
+
+class ServerGenomeBlaster:
+
+    # TODO make a Genome Blaster class?
+    def __init__(self, save_path, queries_path, taxa_blast_order,
+                 complete_reference_species, genome_storage_path):
+        pass
+
+    def download_new_genomes(self, exon_pull_dir):
+        pass
+
+    def blast_genomes(self, expect_value: str):
+        pass
+
+    # I can probably integrate this by early next week
