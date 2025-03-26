ed279596298d6349c1f01b782982ea90d3171016
tonyxieuoft <tony.xhx@gmail.com>
2025-03-25 20:45:50 -0400

    phylo_analysis small change

--- run__phylo_analysis.py
+++ run__phylo_analysis.py
@@ -6,13 +6,15 @@ if __name__ == "__main__":
 
     print("Enter directory containing fasta files of unaligned sequences")
     dir_path = get_generic_directory()
+    # /crun/tony.xie/Downloads/phylo_accel/GeMoMa_vision_results
 
     print("Enter reference sequence converted_NEPR dir")
     reference_path = get_generic_directory()
+    # /crun/tony.xie/Downloads/official_results/converted_NEPR
 
     # just play around in
     wd = "/crun/tony.xie/Downloads/phylo_accel"
-    analyser = Analyser("/crun/tony.xie/Downloads/alignments_15", wd)
+    analyser = Analyser("/crun/tony.xie/Downloads/official_results/alignments8", wd)
     # exon2 = Analyser(alignment_path3, wd)
 
     # full._align_against(exon1, csv_for_r)
